ec438ed82152b4afe940c195bbb325bc5a09e665
Igopk <101907629+Igopk@users.noreply.github.com>
2022-03-25 00:08:32 -0400

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.environ.get('bot_token','5175117165:AAEb4We6cUOSnaXCTE8bACR_IEcjUlTEtYA')
+BOT_TOKEN = os.environ.get('5175117165:AAEb4We6cUOSnaXCTE8bACR_IEcjUlTEtYA')
 TG_API_ID = os.environ.get('14681595',0)
 TG_API_HASH = os.environ.get('tg_api_hash','a86730aab5c59953c424abb4396d32d5')
 TG_ADMIN_USER = os.environ.get('tg_admin_user','nautaii')
